70be21366a1f849a7e3f10c75bada396925d19e1
vetronog <vetronog@yandex.ru>
2016-10-18 15:05:55 +0300

    Небольшие изменения

--- Vector/Vector/Vector.cpp
+++ Vector/Vector/Vector.cpp
@@ -10,25 +10,7 @@ using namespace std;
 int _tmain()
 {
 	vector<double> numbers(istream_iterator<double>(cin), (istream_iterator<double>()));
-	/*while (!cin.eof())
-	{
-		double number;
-		if (cin >> number)
-		{
-			numbers.push_back(number);
-		}
-		else
-		{
-			break;
-		}
-	}*/
-
-	//copy(istream_iterator<double>(cin), istream_iterator<double>(), back_inserter(numbers));
-
-	/*for (auto number : numbers)
-	{
-		cout << number << ", ";
-	}*/
+	
 	ProcessVector(numbers);
 	sort(numbers.begin(), numbers.end());
 	copy(numbers.begin(), numbers.end(), ostream_iterator<double>(cout, ", "));
