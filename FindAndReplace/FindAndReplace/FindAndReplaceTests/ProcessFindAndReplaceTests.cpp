d15fbd1918930f0c1d1bfe23957d0fb2ed6018d7
vetronog <vetronog@yandex.ru>
2016-10-26 10:13:50 +0300

    Update ProcessFindAndReplaceTests.cpp

--- FindAndReplace/FindAndReplace/FindAndReplaceTests/ProcessFindAndReplaceTests.cpp
+++ FindAndReplace/FindAndReplace/FindAndReplaceTests/ProcessFindAndReplaceTests.cpp
@@ -8,19 +8,19 @@ BOOST_AUTO_TEST_SUITE(ProcessVector_function)
 BOOST_AUTO_TEST_CASE(make_first_test)
 {
 	string subject = "one two th";
-	BOOST_CHECK(FindAndReplace(subject, "two", "cat") == "one cat th");
+	BOOST_CHECK(FindAndReplace(subject, "two", "one") == "one one th");
 }
 
 BOOST_AUTO_TEST_CASE(make_multi_found_test)
 {
 	string subject = "one two two th";
-	BOOST_CHECK(FindAndReplace(subject, "two", "cat") == "one cat cat th");
+	BOOST_CHECK(FindAndReplace(subject, "two", "three") == "one three three th");
 }
 
 BOOST_AUTO_TEST_CASE(make_without_found_test)
 {
 	string subject = "one one one th";
-	BOOST_CHECK(FindAndReplace(subject, "two", "cat") == "one one one th");
+	BOOST_CHECK(FindAndReplace(subject, "two", "one") == "one one one th");
 }
 
 BOOST_AUTO_TEST_CASE(make_empty_replace_test)
